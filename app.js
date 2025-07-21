8ad43a4da93bae3fc8f8f1a89cd3b2215d161cea
Ronit Antil <135525594+RonitAntil@users.noreply.github.com>
2025-07-21 13:30:23 +0530

    Create app.js
    Adding functionalities to the Project using JS

---
+++ app.js
@@ -0,0 +1,38 @@
+const playBtn = document.getElementById('play');
+const audio = new Audio('./assets/song.mp3');
+let isPlaying = false;
+
+playBtn.addEventListener('click', () => {
+  if (!isPlaying) {
+    audio.play();
+    isPlaying = true;
+    playBtn.src = './assets/pause_icon.png';
+  } else {
+    audio.pause();
+    isPlaying = false;
+    playBtn.src = './assets/player_icon3.png';
+  }
+});
+const progressBar = document.querySelector('.progressbar input');
+const startTime = document.querySelectorAll('.time')[0];
+const endTime = document.querySelectorAll('.time')[1];
+
+audio.addEventListener('loadedmetadata', () => {
+  progressBar.max = audio.duration;
+  endTime.textContent = formatTime(audio.duration);
+});
+
+audio.addEventListener('timeupdate', () => {
+  progressBar.value = audio.currentTime;
+  startTime.textContent = formatTime(audio.currentTime);
+});
+
+progressBar.addEventListener('input', () => {
+  audio.currentTime = progressBar.value;
+});
+
+function formatTime(time) {
+  let minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60).toString().padStart(2, '0');
+  return `${minutes}:${seconds}`;
+}
